1d8f7b36611e4aeb9c3994ef22ab33916c0ba803
SergioTales1 <algumemailai3@gmail.com>
2024-09-01 17:35:04 -0300

    scroll to see more added on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { AiFillProfile } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
+import { IoIosArrowDown } from "react-icons/io";
 import heroImg from "../assets/hero-img.png";
 import curriculo from "../assets/curriculo-Sergio-Tales.pdf";
 
 const Hero = () => {
   return (
-    <section className="bg-indigo-100 flex justify-center items-center py-12 px-4">
+    <section className="bg-indigo-100 flex justify-center items-center py-4 xl:py-8 px-4">
       <article className="text-center xl:w-3/4 xl:mx-auto">
         <h1 className="text-gray-700 font-bold text-3xl xl:text-6xl">
           Olá, conheça Sergio Tales:
         </h1>
-        <p className="text-gray-700 py-10 font-medium text-lg xl:w-8/12 xl:mx-auto xl:text-2xl">
+        <p className="text-gray-700 py-10 xl:py-6 font-medium text-lg xl:w-8/12 xl:mx-auto xl:text-2xl">
           Um desenvolvedor web full stack focado no desenvolvimento de
           aplicações únicas com o objetivo de transformar sonhos em realidade.
         </p>
@@ -39,6 +40,15 @@ const Hero = () => {
               Github
             </a>
           </button>
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <a
+            href="#projetos"
+            className="flex items-center justify-center flex-col"
+          >
+            <p className="text-xs xl:text-sm">Arraste para ver mais</p>
+            <IoIosArrowDown className="text-2xl" />
+          </a>
         </div>
       </article>
     </section>
