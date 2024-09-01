9895faf9296429d3ca6acee68540caa3302ff880
SergioTales1 <algumemailai3@gmail.com>
2024-09-01 13:07:29 -0300

    modal position fixed on mobile

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,7 +8,7 @@ export default function Modal({ open, setOpen, video }) {
         className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
       />
 
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+      <div className="fixed top-1/4 xl:inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
